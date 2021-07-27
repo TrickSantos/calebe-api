b7ad8ee2e676c0a842eca70a4db49358ddff7c67
Patrick Adan <patrick.tafa@gmail.com>
2021-07-28 00:21:44 +0044

    ajustes

--- app/Controllers/Http/EquipesController.ts
+++ app/Controllers/Http/EquipesController.ts
@@ -1,8 +1,11 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { cuid } from '@ioc:Adonis/Core/Helpers'
 import Database from '@ioc:Adonis/Lucid/Database'
-import Equipe from 'App/Models/Equipe'
 import Aviso from 'App/Mailers/Aviso'
+import Equipe from 'App/Models/Equipe'
+import S3 from 'App/Services/S3'
+import fs from 'fs'
 
 export default class EquipesController {
   public async index({ response }: HttpContextContract) {
@@ -25,11 +28,11 @@ export default class EquipesController {
               nome: schema.string(),
               igrejaId: schema.number([rules.exists({ column: 'id', table: 'igrejas' })]),
               instagram: schema.string(),
-              avatar: schema.string.optional(),
+              avatar: schema.file.optional({ extnames: ['jpg', 'png'], size: '10mb' }),
             }),
             lider: schema.object().members({
               email: schema.string({}, [rules.email()]),
-              avatar: schema.string.optional(),
+              avatar: schema.file.optional({ extnames: ['jpg', 'png'], size: '10mb' }),
               password: schema.string(),
               nome: schema.string(),
               cpf: schema.string({}, [rules.maxLength(11), rules.minLength(11)]),
@@ -39,24 +42,28 @@ export default class EquipesController {
             'equipe.nome': 'O nome da equipe precisa ser informado',
             'equipe.igrejaId': 'A igreja precisa ser informada',
             'equipe.instagram': 'O @ da equipe precisa ser informada',
+            'equipe.avatar.extnames': 'A imagem precisa ser .jpg ou .png',
+            'equipe.avatar.size': 'A imagem pode ter no maximo 10mb',
             'lider.nome': 'O nome da equipe precisa ser informado',
             'lider.cpf.required': 'O CPF precisa ser informado',
             'lider.cpf.maxLength': 'CPF invalido',
             'lider.cpf.minLength': 'CPF invalido',
             'lider.email.required': 'O email do lider precisa ser informado',
             'lider.email.email': 'O email do lider precisa ser valido',
+            'lider.avatar.extnames': 'A imagem precisa ser .jpg ou .png',
+            'lider.avatar.size': 'A imagem pode ter no maximo 10mb',
             'lider.password.required': 'A senha precisa ser informada',
           },
         })
         .then(async (data) => {
           await Database.transaction(async (trx) => {
             const equipe = new Equipe()
-            equipe.merge(data.equipe)
+            equipe.merge({ ...data.equipe, avatar: '' })
             equipe.useTransaction(trx)
             await equipe.save()
             await equipe
               .related('membros')
-              .create({ ...data.lider, perfil: 'lider' })
+              .create({ ...data.lider, perfil: 'lider', avatar: '' })
               .then(async (lider) => {
                 await trx.commit()
                 await new Aviso(lider, equipe).send()
@@ -83,7 +90,8 @@ export default class EquipesController {
       await Equipe.findOrFail(id).then(async (equipe) => {
         await equipe.load('igreja', (query) => query.preload('distrito'))
         await equipe.load('membros')
-        response.send(equipe)
+
+        return response.send(equipe)
       })
     } catch (error) {
       return response.status(500).send(error.message)
@@ -98,18 +106,32 @@ export default class EquipesController {
             nome: schema.string.optional(),
             igrejaId: schema.number.optional([rules.exists({ column: 'id', table: 'igrejas' })]),
             instagram: schema.string.optional(),
-            avatar: schema.string.optional(),
+            avatar: schema.file.optional({ extnames: ['jpg', 'png'], size: '10mb' }),
           }),
           messages: {
-            nome: 'O nome da equipe precisa ser informado',
-            igrejaId: 'A igreja precisa ser informada',
-            instagram: 'O @ da equipe precisa ser informada',
+            'nome': 'O nome da equipe precisa ser informado',
+            'igrejaId': 'A igreja precisa ser informada',
+            'instagram': 'O @ da equipe precisa ser informada',
+            'avatar.extnames': 'A imagem precisa ser .jpg ou .png',
+            'avatar.size': 'A imagem pode ter no maximo 10mb',
           },
         })
         .then(async (data) => {
           const { id } = params
           await Equipe.findOrFail(id).then(async (equipe) => {
-            equipe.merge(data)
+            let avatar = equipe.avatar
+            if (data.avatar && data.avatar.type && data.avatar.tmpPath) {
+              await S3.upload(
+                fs.createReadStream(data.avatar.tmpPath),
+                `${cuid()}.${data.avatar.extname}`,
+                data.avatar.type,
+                'equipe',
+                'public-read'
+              ).then((url) => {
+                avatar = url
+              })
+            }
+            equipe.merge({ ...data, avatar })
             await equipe.save()
             return response.send(equipe)
           })
--- build/app/Controllers/Http/EquipesController.js
+++ build/app/Controllers/Http/EquipesController.js
@@ -4,9 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Validator_1 = global[Symbol.for('ioc.use')]("Adonis/Core/Validator");
+const Helpers_1 = global[Symbol.for('ioc.use')]("Adonis/Core/Helpers");
 const Database_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Lucid/Database"));
-const Equipe_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Models/Equipe"));
 const Aviso_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Mailers/Aviso"));
+const Equipe_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Models/Equipe"));
+const S3_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Services/S3"));
+const fs_1 = __importDefault(require("fs"));
 class EquipesController {
     async index({ response }) {
         try {
@@ -28,11 +31,11 @@ class EquipesController {
                         nome: Validator_1.schema.string(),
                         igrejaId: Validator_1.schema.number([Validator_1.rules.exists({ column: 'id', table: 'igrejas' })]),
                         instagram: Validator_1.schema.string(),
-                        avatar: Validator_1.schema.string.optional(),
+                        avatar: Validator_1.schema.file.optional({ extnames: ['jpg', 'png'], size: '10mb' }),
                     }),
                     lider: Validator_1.schema.object().members({
                         email: Validator_1.schema.string({}, [Validator_1.rules.email()]),
-                        avatar: Validator_1.schema.string.optional(),
+                        avatar: Validator_1.schema.file.optional({ extnames: ['jpg', 'png'], size: '10mb' }),
                         password: Validator_1.schema.string(),
                         nome: Validator_1.schema.string(),
                         cpf: Validator_1.schema.string({}, [Validator_1.rules.maxLength(11), Validator_1.rules.minLength(11)]),
@@ -42,24 +45,28 @@ class EquipesController {
                     'equipe.nome': 'O nome da equipe precisa ser informado',
                     'equipe.igrejaId': 'A igreja precisa ser informada',
                     'equipe.instagram': 'O @ da equipe precisa ser informada',
+                    'equipe.avatar.extnames': 'A imagem precisa ser .jpg ou .png',
+                    'equipe.avatar.size': 'A imagem pode ter no maximo 10mb',
                     'lider.nome': 'O nome da equipe precisa ser informado',
                     'lider.cpf.required': 'O CPF precisa ser informado',
                     'lider.cpf.maxLength': 'CPF invalido',
                     'lider.cpf.minLength': 'CPF invalido',
                     'lider.email.required': 'O email do lider precisa ser informado',
                     'lider.email.email': 'O email do lider precisa ser valido',
+                    'lider.avatar.extnames': 'A imagem precisa ser .jpg ou .png',
+                    'lider.avatar.size': 'A imagem pode ter no maximo 10mb',
                     'lider.password.required': 'A senha precisa ser informada',
                 },
             })
                 .then(async (data) => {
                 await Database_1.default.transaction(async (trx) => {
                     const equipe = new Equipe_1.default();
-                    equipe.merge(data.equipe);
+                    equipe.merge({ ...data.equipe, avatar: '' });
                     equipe.useTransaction(trx);
                     await equipe.save();
                     await equipe
                         .related('membros')
-                        .create({ ...data.lider, perfil: 'lider' })
+                        .create({ ...data.lider, perfil: 'lider', avatar: '' })
                         .then(async (lider) => {
                         await trx.commit();
                         await new Aviso_1.default(lider, equipe).send();
@@ -87,7 +94,7 @@ class EquipesController {
             await Equipe_1.default.findOrFail(id).then(async (equipe) => {
                 await equipe.load('igreja', (query) => query.preload('distrito'));
                 await equipe.load('membros');
-                response.send(equipe);
+                return response.send(equipe);
             });
         }
         catch (error) {
@@ -102,18 +109,26 @@ class EquipesController {
                     nome: Validator_1.schema.string.optional(),
                     igrejaId: Validator_1.schema.number.optional([Validator_1.rules.exists({ column: 'id', table: 'igrejas' })]),
                     instagram: Validator_1.schema.string.optional(),
-                    avatar: Validator_1.schema.string.optional(),
+                    avatar: Validator_1.schema.file.optional({ extnames: ['jpg', 'png'], size: '10mb' }),
                 }),
                 messages: {
-                    nome: 'O nome da equipe precisa ser informado',
-                    igrejaId: 'A igreja precisa ser informada',
-                    instagram: 'O @ da equipe precisa ser informada',
+                    'nome': 'O nome da equipe precisa ser informado',
+                    'igrejaId': 'A igreja precisa ser informada',
+                    'instagram': 'O @ da equipe precisa ser informada',
+                    'avatar.extnames': 'A imagem precisa ser .jpg ou .png',
+                    'avatar.size': 'A imagem pode ter no maximo 10mb',
                 },
             })
                 .then(async (data) => {
                 const { id } = params;
                 await Equipe_1.default.findOrFail(id).then(async (equipe) => {
-                    equipe.merge(data);
+                    let avatar = equipe.avatar;
+                    if (data.avatar && data.avatar.type && data.avatar.tmpPath) {
+                        await S3_1.default.upload(fs_1.default.createReadStream(data.avatar.tmpPath), `${Helpers_1.cuid()}.${data.avatar.extname}`, data.avatar.type, 'equipe', 'public-read').then((url) => {
+                            avatar = url;
+                        });
+                    }
+                    equipe.merge({ ...data, avatar });
                     await equipe.save();
                     return response.send(equipe);
                 });
